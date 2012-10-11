6d3373bfd385d3ab173741909377b0c3b9b54ace
wisut hantanong <wizzup@wizzup.com>
2012-10-11 21:19:04 +0700

    current macbook config snapshots

--- xmonad.hs
+++ xmonad.hs
@@ -24,8 +24,7 @@ myTerminal      = "urxvt"
  
 -- Width of the window border in pixels.
 --
--- myBorderWidth   = 1
-myBorderWidth   = 2
+myBorderWidth   = 1
  
 -- modMask lets you specify which modkey you want to use. The default
 -- is mod1Mask ("left alt").  You may also consider using mod3Mask
@@ -190,7 +189,8 @@ myMouseBindings (XConfig {XMonad.modMask = modMask}) = M.fromList $
 -- The available layouts.  Note that each layout is separated by |||,
 -- which denotes layout choice.
 --
-myLayout = tiled ||| Mirror tiled ||| Full
+-- myLayout = tiled ||| Mirror tiled ||| Full
+myLayout = tiled ||| Full
   where
      -- default tiling algorithm partitions the screen into two panes
      tiled   = Tall nmaster delta ratio
@@ -199,7 +199,7 @@ myLayout = tiled ||| Mirror tiled ||| Full
      nmaster = 1
  
      -- Default proportion of screen occupied by master pane
-     ratio   = 1/2
+     ratio   = 2/3
  
      -- Percent of screen to increment by when resizing panes
      delta   = 3/100
